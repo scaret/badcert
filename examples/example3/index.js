c92715252a5792f0898f7cd0f8891a3727f1d976
scaret <scaret.in@gmail.com>
2021-02-24 20:58:44 +0800

    turn to light-server

---
+++ examples/example3/index.js
@@ -0,0 +1,27 @@
+const Koa = require('koa')
+const Router = require('koa-router')
+const serve = require('koa-static')
+const historyFallback = require('koa2-history-api-fallback')
+
+const app = new Koa()
+const router = new Router()
+
+router.get('/api/echo/*', (ctx) => {
+  // ctx.router available
+  ctx.body = 'Echo from server: ' + ctx.req.url
+})
+
+router.get('/auth/*', (ctx) => {
+  // ctx.router available
+  ctx.body = 'Auth check from server: ' + ctx.req.url
+})
+
+app.use(router.routes())
+  .use(router.allowedMethods())
+
+app.use(historyFallback())
+app.use(serve('public'))
+
+app.listen(8010, () => {
+  console.log('koa server listening on http://localhost:8010')
+})
